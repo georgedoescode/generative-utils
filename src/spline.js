776e805c41cfc3d53af81df717735fb93c1808f0
John F Morton <morton@jmx2.com>
2023-12-22 10:13:23 -0600

    Updated revised spline func to account for tension

--- src/spline.js
+++ src/spline.js
@@ -1,69 +1,68 @@
-function formatPoints(points, close) {
-  points = [...points];
+function spline(points = [], tension = 0.5, close = false, cb) {
 
-  if (!Array.isArray(points[0])) {
-    points = points.map(({ x, y }) => [x, y]);
-  }
+    let tensionInv  = tension * 0.5;
+    if (points.length < 2) return ''
 
-  if (close) {
-    const lastPoint = points[points.length - 1];
-    const secondToLastPoint = points[points.length - 2];
+    // Helper function to calculate a point on the curve
+    function catmullRom(p0, p1, p2, p3, t, tension) {
+        const t2 = t * t
+        const t3 = t2 * t
 
-    const firstPoint = points[0];
-    const secondPoint = points[1];
+        // Calculate tension vectors
+        const m0 = {
+            x: (p2.x - p0.x) * tension,
+            y: (p2.y - p0.y) * tension,
+        }
+        const m1 = {
+            x: (p3.x - p1.x) * tension,
+            y: (p3.y - p1.y) * tension,
+        }
 
-    points.unshift(lastPoint);
-    points.unshift(secondToLastPoint);
+        // Calculate the point
+        return {
+            x:
+                (2 * p1.x - 2 * p2.x + m0.x + m1.x) * t3 +
+                (-3 * p1.x + 3 * p2.x - 2 * m0.x - m1.x) * t2 +
+                m0.x * t +
+                p1.x,
+            y:
+                (2 * p1.y - 2 * p2.y + m0.y + m1.y) * t3 +
+                (-3 * p1.y + 3 * p2.y - 2 * m0.y - m1.y) * t2 +
+                m0.y * t +
+                p1.y,
+        }
+    }
 
-    points.push(firstPoint);
-    points.push(secondPoint);
-  }
+    let path = 'M' + [points[0].x, points[0].y]
+    cb && cb('MOVE', [points[0].x, points[0].y])
 
-  return points.flat();
-}
+    const numPoints = points.length
+    const segmentCount = 20 // Number of segments between control points
+    const loopLimit = close ? numPoints : numPoints - 1
 
-function spline(points = [], tension = 1, close = false, cb) {
-  points = formatPoints(points, close);
+    for (let i = 0; i < loopLimit; i++) {
+        const p0 = points[i === 0 ? (close ? numPoints - 1 : i) : i - 1]
+        const p1 = points[i]
+        const p2 = points[(i + 1) % numPoints]
+        const p3 =
+            points[
+                i + 2 < numPoints ? i + 2 : close ? (i + 2) % numPoints : i + 1
+            ]
 
-  const size = points.length;
-  const last = size - 4;
+        for (let j = 1; j <= segmentCount; j++) {
+            const t = j / segmentCount
+            const pt = catmullRom(p0, p1, p2, p3, t, tensionInv)
 
-  const startPointX = close ? points[2] : points[0];
-  const startPointY = close ? points[3] : points[1];
+            path += 'L' + [pt.x, pt.y]
+            cb && cb('LINE', [pt.x, pt.y])
+        }
+    }
 
-  let path = "M" + [startPointX, startPointY];
+    if (close) {
+        path += 'Z'
+    }
 
-  cb && cb("MOVE", [startPointX, startPointY]);
-
-  const startIteration = close ? 2 : 0;
-  const maxIteration = close ? size - 4 : size - 2;
-  const inc = 2;
-
-  for (let i = startIteration; i < maxIteration; i += inc) {
-    const x0 = i ? points[i - 2] : points[0];
-    const y0 = i ? points[i - 1] : points[1];
-
-    const x1 = points[i + 0];
-    const y1 = points[i + 1];
-
-    const x2 = points[i + 2];
-    const y2 = points[i + 3];
-
-    const x3 = i !== last ? points[i + 4] : x2;
-    const y3 = i !== last ? points[i + 5] : y2;
-
-    const cp1x = x1 + ((x2 - x0) / 6) * tension;
-    const cp1y = y1 + ((y2 - y0) / 6) * tension;
-
-    const cp2x = x2 - ((x3 - x1) / 6) * tension;
-    const cp2y = y2 - ((y3 - y1) / 6) * tension;
-
-    path += "C" + [cp1x, cp1y, cp2x, cp2y, x2, y2];
-
-    cb && cb("CURVE", [cp1x, cp1y, cp2x, cp2y, x2, y2]);
-  }
-
-  return path;
+    return path
 }
 
 export { spline };
